e9e9e4cc679fe934a91a6ad469a8d6c5a480eb7d
aScHEABL <natothun@gmail.com>
2022-04-08 02:23:10 +0800

    feat: complete 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
-
+/*test 1
+const removeFromArray = function(array, targetElement) {
+for (let i = 0; i < array.length; i++) {
+    switch (true) {
+    case array[i - 1] === targetElement:
+        array.splice(i - 1, 1);
+        break;
+}
+}
+return array;
 };
+*/
+
+const removeFromArray = function(array, ...targetElement) {
+    for (let targetElementIteration = 0; targetElementIteration < targetElement.length; targetElementIteration++) {
+        for (arrayIteration = 0; arrayIteration < array.length; arrayIteration++) {
+            switch (true) {
+                case array[arrayIteration] === targetElement[targetElementIteration]:
+                array.splice(arrayIteration, 1);
+                break;
+            }
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
