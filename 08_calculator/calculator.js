e519e991c0d808c68550f512e33a08d292ec2572
aScHEABL <natothun@gmail.com>
2022-04-09 21:25:47 +0800

    feat: has not complete function factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,57 @@
-const add = function() {
-	
+const add = function(...num) {
+  let sum = 0;
+	for (let i = 0; i < num.length; i++) {
+    switch (true) {
+      case num[i] > num[num.length - 1]:
+        let temporaryPlaceHolder = num[i];
+        num[i] = num[num.length - 1];
+        num[num.length - 1] = temporaryPlaceHolder;
+        break;
+    }
+    sum += num[i];
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = 1;
+  for (let i = 0; i <= array.length - 1; i++) {
+    sum *= array[i]
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(baseNum, iteration) {
+  let sum = baseNum;
+	for (let i = 0; i < iteration - 1; i++) {
+    sum *= baseNum 
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNum) {
+  if (factorialNum === 0) {
+    factorialNum = 1;
+  }
+    let sum = 1;
+  for (let i = factorialNum; i > 0; i--) {
+    console.log(i);
+    sum *= i;
+    factorial(i - 1);
+  }
+  return sum;
 };
 
 // Do not edit below this line
