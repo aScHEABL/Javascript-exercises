f97e2cbd9397efddcd2f8ad913a0cc3e795e25fb
aScHEABL <natothun@gmail.com>
2022-04-10 17:35:08 +0800

    feat: complete 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+text = text.toLowerCase();
+text = text.replaceAll("!", "");
+text = text.replaceAll(",", "");
+text = text.replaceAll(" ", "");
+text = text.replaceAll(".", "");
+console.log(text);
+let reverseText = "";
+console.log(reverseText);
+for (let i = text.length; i > 0; i--) {
+    reverseText += text.charAt(i - 1);
+}
+if (reverseText === text) {
+    return true;
+} else if (reverseText !== text) {
+    return false;
+}
 };
+
+function reverseString(text) {
+}
 
 // Do not edit below this line
 module.exports = palindromes;
