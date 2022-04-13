a9ce9fe2b8d07407c28f961fa8d1cc242dc70ebd
aScHEABL <natothun@gmail.com>
2022-04-13 12:02:29 +0800

    feat: complete 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(fibonacciIndex) {
+    const fibonacciArray = [0,1,1];
+    fibonacciIndex = +fibonacciIndex;
+    if (fibonacciIndex < 0) {
+        return "OOPS";
+    } else if (fibonacciIndex < 3) {
+        return fibonacciArray[fibonacciIndex];
+    } else if (fibonacciIndex >= 3) {
+        for (let i = 3; i <=fibonacciIndex; i++) {
+            let fibonacciNum = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+            fibonacciArray.push(fibonacciNum);
+        }
+    }
+    return fibonacciArray[fibonacciIndex];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
