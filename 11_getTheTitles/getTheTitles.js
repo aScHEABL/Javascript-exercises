78e8a101c0c15f87fcb7381bc7113b0c3da3b436
aScHEABL <natothun@gmail.com>
2022-04-19 00:02:28 +0800

    feat: complete 11-getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksMetadata) {
+const titleOfTheBooks = booksMetadata.map((booksMetadata) => booksMetadata.title);
+console.log(titleOfTheBooks);
+return titleOfTheBooks;
 };
 
 // Do not edit below this line
