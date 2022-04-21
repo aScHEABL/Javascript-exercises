42979db0423160aeebfcaf1eaa9f88e0854b4d40
aScHEABL <natothun@gmail.com>
2022-04-21 17:29:39 +0800

    feat: complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,21 +1,22 @@
 const findTheOldest = function(listOfPeople) {
-const mapAges = listOfPeople.map((age) => age.yearOfDeath - age.yearOfBirth);
-console.log(mapAges);
-const sortAges = mapAges.sort((a, b) => b - a);
-console.log(sortAges);
-const filterTheNonOldest = listOfPeople.filter((theOldestPerson) => theOldestPerson.yearOfDeath - theOldestPerson.yearOfBirth === sortAges[0]);
-console.log(typeof filterTheNonOldest);
-console.log(Array.isArray(filterTheNonOldest));
-console.log(filterTheNonOldest[0]);
-return filterTheNonOldest[0];
+    let currentTime = new Date();
+    let currentYear = currentTime.getFullYear();
+    listOfPeople.forEach(function(checkIfAlive) {
+        if (isNaN(checkIfAlive.yearOfDeath)) {
+            checkIfAlive.yearOfDeath = currentYear;
+        }
+    });
+    console.log(listOfPeople);
+    const mapAges = listOfPeople.map((age) => age.yearOfDeath - age.yearOfBirth);
+    console.log(mapAges);
+    const sortAges = mapAges.sort((a, b) => b - a);
+    console.log(sortAges);
+    const filterTheNonOldest = listOfPeople.filter((theOldestPerson) => theOldestPerson.yearOfDeath - theOldestPerson.yearOfBirth === sortAges[0]);
+    console.log(typeof filterTheNonOldest);
+    console.log(Array.isArray(filterTheNonOldest));
+    console.log(filterTheNonOldest[0]);
+    return filterTheNonOldest[0];
 };
-/*
-const mapTheOldest = filterTheNonOldest.map((theOldestPerson) => theOldestPerson.name);
-console.log(mapTheOldest);
-const theOldestPersonName = mapTheOldest.toString();
-console.log(theOldestPersonName);
-return theOldestPersonName;
-*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
