7f5ddd5e2f1edb21fae9a2f1ba8209378bff7799
aScHEABL <natothun@gmail.com>
2022-04-21 13:13:42 +0800

    feat: complete the first test of findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(listOfPeople) {
+const mapAges = listOfPeople.map((age) => age.yearOfDeath - age.yearOfBirth);
+console.log(mapAges);
+const sortAges = mapAges.sort((a, b) => b - a);
+console.log(sortAges);
+const filterTheNonOldest = listOfPeople.filter((theOldestPerson) => theOldestPerson.yearOfDeath - theOldestPerson.yearOfBirth === sortAges[0]);
+console.log(typeof filterTheNonOldest);
+console.log(Array.isArray(filterTheNonOldest));
+console.log(filterTheNonOldest[0]);
+return filterTheNonOldest[0];
 };
+/*
+const mapTheOldest = filterTheNonOldest.map((theOldestPerson) => theOldestPerson.name);
+console.log(mapTheOldest);
+const theOldestPersonName = mapTheOldest.toString();
+console.log(theOldestPersonName);
+return theOldestPersonName;
+*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
