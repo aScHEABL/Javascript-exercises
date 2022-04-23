c9bc2452612d8881c00e5672c3d558e101fd6c06
aScHEABL <natothun@gmail.com>
2022-04-23 10:30:56 +0800

    add: partially complete 13-caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,20 @@
-const caesar = function() {
-
+const caesar = function (string, shift) {
+  const breakDownTheString = [];
+  let shifttedString = "";
+  for (let i = 0; i < string.length; i++) {
+    breakDownTheString.push(string[i]);
+  }
+  //breakDownTheString.forEach((character) => shifttedString += String.fromCharCode(character.codePointAt(0) + shift));
+  breakDownTheString.forEach(function (character) {
+    if (!character.match(/[a-zA-Z]/)) {
+      shifttedString += character;
+    } else {
+      shifttedString += String.fromCharCode(character.codePointAt(0) + shift);
+    }
+  });
+  console.log(breakDownTheString);
+  console.log(shifttedString);
+  return shifttedString;
 };
 
 // Do not edit below this line
